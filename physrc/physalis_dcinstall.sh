bbd2eae45120593935c2e114d9cae36851f68e6a
Sam Mahonri <64850697+sam-mahonri@users.noreply.github.com>
2024-04-23 18:30:39 -0300

    fix: uninstall discord and optimization

--- physrc/physalis_dcinstall.sh
+++ physrc/physalis_dcinstall.sh
@@ -12,62 +12,96 @@ print_success() {
 }
 
 print_error() {
-    echo -e "${COLOR_ERROR}- [Error]${COLOR_RESET}"
+    echo -e "${COLOR_ERROR}- [Erro]${COLOR_RESET}"
     exit 1
 }
 
 clear_all(){
     clear
-    echo -e "${COLOR_SUCCESS}2024 (c) Sam Mahonri - Physalis Tools - Discord Installer - ${COLOR_WARNING}[CTRL+C] to Cancel${COLOR_RESET}"
+    echo -e "${COLOR_SUCCESS}2024 (c) Sam Mahonri - Physalis Tools - Discord Installer - ${COLOR_WARNING}[CTRL+C] para Cancelar${COLOR_RESET}"
     echo -e "------------------------------------------------------------------------------\n"
+}
+
+adaptive_version_ref() {
+    local version=$1
+    version_breaker=""
+    discord_formatted=""
+
+    if [ "$version" = "stable" ]; then
+        version_breaker=""
+        discord_formatted=""
+    else
+        version_breaker="-canary"
+        discord_formatted="Canary"
+    fi
 }
 
 uninstall_discord() {
     local version=$1
-    if [ "$version" = "stable" ]; then
-        discord_formated=""
-    else
-        discord_formated="Canary"
-    fi
-    # Stop and disable Discord service
-    echo -n "- Stopping and disabling Discord service... "
-    sudo systemctl stop discord-$version.service >/dev/null 2>&1
-    sudo systemctl disable discord-$version.service >/dev/null 2>&1
+    adaptive_version_ref "$version"
+
+
+    # Parar e desativar o serviço do Discord
+    echo -n "- Parando e desativando o serviço do Discord... "
+    sudo systemctl stop discord$version_breaker.service >/dev/null 2>&1
+    sudo systemctl disable discord$version_breaker.service >/dev/null 2>&1
     print_success
 
-    # Remove Discord service file
-    echo -n "- Removing Discord service file... "
-    sudo rm -f "/etc/systemd/system/discord-$version.service" >/dev/null 2>&1
+    # Remover arquivo de serviço do Discord
+    echo -n "- Removendo arquivo de serviço do Discord... "
+    sudo rm -f "/etc/systemd/system/discord$version_breaker.service" >/dev/null 2>&1
     print_success
 
-    # Remove Discord installation directory
-    echo -n "- Removing Discord installation directory... "
-    sudo rm -rf "/opt/Discord$discord_formated"
+    # Remover diretório de instalação do Discord
+    echo -n "- Removendo diretório de instalação do Discord... "
+    sudo rm -rf "/opt/Discord$discord_formatted"
     print_success
 
-    echo -e "\n${COLOR_SUCCESS}Discord ($version) has been completely uninstalled!${COLOR_RESET}\n"
+    # Remover o aplicativo dos menus
+    echo -n "- Removendo o aplicativo dos menus... "
+    sudo rm -f "/usr/share/applications/discord$version_breaker.desktop" >/dev/null 2>&1
+    print_success
+
+    echo -e "\n${COLOR_SUCCESS}O Discord foi completamente desinstalado!${COLOR_RESET}\n"
     exit 0
+}
+
+add_to_menus() {
+    local version=$1
+    adaptive_version_ref "$version"
+    echo -n "- Adicionando o aplicativo ao menu... "
+    cat << EOF | sudo tee "/usr/share/applications/discord$version_breaker.desktop" >/dev/null
+[Desktop Entry]
+Name=Discord $version
+Comment=All-in-one voice and text chat for gamers
+Exec=/opt/Discord$discord_formated/Discord$discord_formated
+Icon=/opt/Discord$discord_formated/discord.png
+Terminal=false
+Type=Application
+Categories=Network;InstantMessaging;Game;
+EOF
+    print_success
 }
 
 clear_all
 
-# Check if Discord process is running
+# Verificar se o processo do Discord está em execução
 if pgrep -f "/opt/Discord" >/dev/null; then
-    echo -e "${COLOR_WARNING}- Discord is currently running. Please close Discord before proceeding with the (un)installation.\n${COLOR_RESET}"
+    echo -e "${COLOR_WARNING}- Discord está atualmente em execução. Por favor, feche o Discord antes de prosseguir com a (des)instalação.\n${COLOR_RESET}"
     exit 1
 fi
 
 sudo -v
 
-# Ask user whether to install or uninstall Discord
-echo -e "${COLOR_INFO}Do you want to install or uninstall Discord?${COLOR_RESET}"
-select action in "Install" "Uninstall"; do
+# Perguntar ao usuário se deseja instalar ou desinstalar o Discord
+echo -e "${COLOR_INFO}Você deseja instalar ou desinstalar o Discord?${COLOR_RESET}"
+select action in "Instalar" "Desinstalar"; do
     case $action in
-        Install)
+        Instalar)
             break;;
-        Uninstall)
-            # Ask user which version to uninstall
-            echo -e "${COLOR_INFO}Which version of Discord do you want to uninstall?${COLOR_RESET}"
+        Desinstalar)
+            # Perguntar ao usuário qual versão desinstalar
+            echo -e "${COLOR_INFO}Qual versão do Discord você deseja desinstalar?${COLOR_RESET}"
             select version in "Stable" "Canary"; do
                 case $version in
                     Stable)
@@ -75,39 +109,35 @@ select action in "Install" "Uninstall"; do
                     Canary)
                         uninstall_discord "canary";;
                     *)
-                        echo "${COLOR_ERROR}Invalid option. Please select either 1 or 2.${COLOR_RESET}";;
+                        echo "${COLOR_ERROR}Opção inválida. Por favor, selecione 1 ou 2.${COLOR_RESET}";;
                 esac
             done;;
         *)
-            echo "${COLOR_ERROR}Invalid option. Please select either 1 or 2.${COLOR_RESET}";;
+            echo "${COLOR_ERROR}Opção inválida. Por favor, selecione 1 ou 2.${COLOR_RESET}";;
     esac
 done
 
 clear_all
 
-echo -e "${COLOR_INFO}This script fetches the latest official version of Discord via its API and facilitates the installation of either the Canary or Stable version. It ensures correct installation on most Linux systems.${COLOR_RESET}\n"
+echo -e "${COLOR_INFO}Este script busca a versão oficial mais recente do Discord via sua API e facilita a instalação da versão Estável ou Canary. Ele garante a instalação correta na maioria dos sistemas Linux.${COLOR_RESET}\n"
 
-# Ask user if they want to continue
-read -p "Do you want to continue with the installation by removing the current version (if any)? (y/n): " continue_install
-if [[ $continue_install != "y" ]]; then
-    echo -e "${COLOR_WARNING}- Installation aborted.${COLOR_RESET}"
+# Perguntar ao usuário se deseja continuar com a instalação
+read -p "Você deseja continuar com a instalação removendo a versão atual (se houver)? (s/n): " continue_install
+if [[ $continue_install != "s" ]]; then
+    echo -e "${COLOR_WARNING}- Instalação abortada.${COLOR_RESET}"
     exit 1
 fi
 
-# Select Discord version (Stable or Canary)
-read -p "Which version of Discord do you want to install? (stable/canary): " discord_version
+# Selecionar versão do Discord (Estável ou Canary)
+read -p "Qual versão do Discord você deseja instalar? (stable/canary): " discord_version
 if [[ "$discord_version" != "stable" && "$discord_version" != "canary" ]]; then
-    echo -e "${COLOR_ERROR}- Invalid Discord version. Please choose 'stable' or 'canary'.${COLOR_RESET}"
+    echo -e "${COLOR_ERROR}- Versão do Discord inválida. Por favor, escolha 'stable' ou 'canary'.${COLOR_RESET}"
     exit 1
 fi
 
-if [ "$discord_version" = "stable" ]; then
-    discord_formated=""
-else
-    discord_formated="Canary"
-fi
+adaptive_version_ref "$discord_version"
 
-# Check which package manager the current distribution uses
+# Verificar qual gerenciador de pacotes a distribuição atual utiliza
 if command -v dnf >/dev/null; then
     package_manager="dnf"
 elif command -v apt >/dev/null; then
@@ -115,12 +145,12 @@ elif command -v apt >/dev/null; then
 elif command -v pacman >/dev/null; then
     package_manager="pacman"
 else
-    echo -e "${COLOR_ERROR}- Unsupported package manager. Please install the dependencies manually.${COLOR_RESET}"
+    echo -e "${COLOR_ERROR}- Gerenciador de pacotes não suportado. Por favor, instale as dependências manualmente.${COLOR_RESET}"
     exit 1
 fi
 
-# Install dependencies
-echo -n "- Installing dependencies... "
+# Instalar dependências
+echo -n "- Instalando dependências... "
 if [ "$package_manager" = "dnf" ]; then
     if sudo dnf install -y libatomic libcxx libdbusmenu-gtk2 libindicator libappindicator GConf2; then
         print_success
@@ -141,42 +171,45 @@ elif [ "$package_manager" = "pacman" ]; then
     fi
 fi
 
-# Remove previous Discord installation directory if exists
-echo -n "- Removing previous Discord installation directory... "
+# Remover diretório de instalação do Discord anterior, se existir
+echo -n "- Removendo diretório de instalação do Discord anterior... "
 if [ -d "/opt/Discord$discord_formated" ]; then
     sudo rm -rf "/opt/Discord$discord_formated"
     print_success
 else
-    echo -e "${COLOR_WARNING}[!] No previous Discord installation found.${COLOR_RESET}"
+    echo -e "${COLOR_WARNING}[!] Nenhuma instalação anterior do Discord encontrada.${COLOR_RESET}"
 fi
 
-# Download the latest version of Discord for Linux
-echo -e "- Downloading the latest version of Discord for Linux...\n"
-if wget -O "/tmp/discord-$discord_version.tar.gz" "https://discord.com/api/download/$discord_version?platform=linux&format=tar.gz"; then
+# Baixar a versão mais recente do Discord para Linux
+echo -e "- Baixando a versão mais recente do Discord para Linux...\n"
+if wget -O "/tmp/discord$version_breaker.tar.gz" "https://discord.com/api/download/$discord_version?platform=linux&format=tar.gz"; then
     print_success
 else
     print_error
 fi
 
-# Extract the latest version of Discord for Linux
-echo -n "- Extracting the latest version of Discord for Linux... "
-if tar -xvf "/tmp/discord-$discord_version.tar.gz" -C "/tmp" >/dev/null; then
+# Extrair a versão mais recente do Discord para Linux
+echo -n "- Extraindo a versão mais recente do Discord para Linux... "
+if tar -xvf "/tmp/discord$version_breaker.tar.gz" -C "/tmp" >/dev/null; then
     print_success
 else
     print_error
 fi
 
-# Move the Discord installation to the /opt directory
-echo -n "- Moving the Discord installation to /opt... "
+# Mover a instalação do Discord para o diretório /opt
+echo -n "- Movendo a instalação do Discord para /opt... "
 if sudo mv "/tmp/Discord$discord_formated" "/opt/Discord$discord_formated"; then
     print_success
 else
     print_error
 fi
 
-# Create the systemd service file for Discord
-echo -n "- Creating the systemd service file for Discord... "
-cat << EOF | sudo tee "/etc/systemd/system/discord-$discord_version.service" >/dev/null
+# Adicionar o aplicativo aos menus
+add_to_menus "$discord_version"
+
+# Criar o arquivo de serviço systemd para o Discord
+echo -n "- Criando o arquivo de serviço systemd para o Discord... "
+cat << EOF | sudo tee "/etc/systemd/system/discord$version_breaker.service" >/dev/null
 [Unit]
 Description=Discord $discord_formated
 After=network.target
@@ -193,7 +226,15 @@ if [ $? -eq 0 ]; then
 else
     print_error
 fi
+sudo systemctl daemon-reload
+sudo systemctl enable discord$version_breaker.service
 
 chmod +x /opt/Discord$discord_formated/Discord$discord_formated
 
-echo -e "\n${COLOR_SUCCESS}Installation and configuration of Discord ($discord_version) completed successfully!${COLOR_RESET}\n"
+echo -e "\n${COLOR_SUCCESS}Instalação e configuração do Discord ($discord_version) concluídas com sucesso!${COLOR_RESET}\n"
+read -p "Deseja iniciar o Discord agora? (s/n): " start_discord
+if [[ $start_discord == "s" ]]; then
+    # Iniciar o Discord em um novo processo
+    nohup /opt/Discord$discord_formated/Discord$discord_formated >/dev/null 2>&1 &
+    echo -e "${COLOR_SUCCESS}Discord iniciado.${COLOR_RESET}"
+fi
